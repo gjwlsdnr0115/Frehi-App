49c70096be7e0816680ee5fc091a6f7c0d5c41bb
Jinwook <gjwlsdnr0115@gmail.com>
2021-07-04 19:56:25 +0900

    Initial Commit

---
+++ Freshi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Freshi
+//
+//  Created by Jinwook Huh on 2021/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
