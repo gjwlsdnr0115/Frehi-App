66acb7df40dfcc6c8525bdbc9aaab2079682cb89
Jinwook <gjwlsdnr0115@gmail.com>
2021-07-05 23:33:27 +0900

    Add CoreData Model

---
+++ FoodEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  FoodEntity+CoreDataClass.swift
+//  Freshi
+//
+//  Created by Jinwook Huh on 2021/07/05.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(FoodEntity)
+public class FoodEntity: NSManagedObject {
+
+}
---
+++ FoodEntity+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  FoodEntity+CoreDataProperties.swift
+//  Freshi
+//
+//  Created by Jinwook Huh on 2021/07/05.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension FoodEntity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<FoodEntity> {
+        return NSFetchRequest<FoodEntity>(entityName: "Food")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var count: Int16
+    @NSManaged public var date: String?
+    @NSManaged public var location: String?
+    @NSManaged public var image: Data?
+
+}
+
+extension FoodEntity : Identifiable {
+
+}
