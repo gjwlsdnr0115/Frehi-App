768827e6c11fa8d41ec381799716bbd47228fb88
Jinwook <gjwlsdnr0115@gmail.com>
2021-07-05 14:42:07 +0900

    Add tableView UI

--- Freshi/HomeViewController.swift
+++ Freshi/HomeViewController.swift
@@ -1,19 +1,22 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Freshi
 //
-//  Created by Jinwook Huh on 2021/07/04.
+//  Created by Jinwook Huh on 2021/07/05.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    
+    
 
 }
-
