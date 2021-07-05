c8d3d921844b0703e56a5e4072178e49f3bd233e
Jinwook <gjwlsdnr0115@gmail.com>
2021-07-05 14:43:44 +0900

    Create custom cell

---
+++ Freshi/FoodInfoCell.swift
@@ -0,0 +1,23 @@
+//
+//  FoodInfoCell.swift
+//  Freshi
+//
+//  Created by Jinwook Huh on 2021/07/05.
+//
+
+import UIKit
+
+class FoodInfoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
